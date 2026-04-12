d5f65a65a2c6db911200107ed7a88a02defa98ef
cameron200202cam-oss <cameron200202cam@gmail.com>
2026-04-11 21:18:33 -0400

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -4,7 +4,6 @@ const { createClient } = require("@supabase/supabase-js");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-
 const loginAttempts = new Map();
 const MAX_LOGIN_ATTEMPTS = 5;
 const LOCKOUT_WINDOW_MS = 15 * 60 * 1000;
