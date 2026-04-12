2798ff48bd9d70f5a6ef5c409ed3de06d4cfa3ae
cameron200202cam-oss <cameron200202cam@gmail.com>
2026-04-11 21:01:47 -0400

    Create test.js

---
+++ api/test.js
@@ -0,0 +1,11 @@
+module.exports = async function handler(req, res) {
+  const url = process.env.SUPABASE_URL;
+  const key = process.env.SUPABASE_SERVICE_KEY;
+  
+  res.status(200).json({
+    hasUrl: !!url,
+    urlValue: url ? url.slice(0, 30) + '...' : 'MISSING',
+    hasKey: !!key,
+    keyStart: key ? key.slice(0, 10) + '...' : 'MISSING'
+  });
+};
